ccb56f5349799f8af1bde2ff19d4ac2cfa67109f
tradyann <tradyann@gmail.com>
2024-05-06 12:56:11 +0400

    carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, RouterLink],
+  imports: [TranslateModule, RouterLink, MdbCarouselModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
+  slides = [
+    {
+      "src": "https://www.equipecofidis.com/medias/articles/desktop-cofidis-prolonge-son-engagement-jusqu-en-2028-picture-20240419090745.jpg",
+      "title": "Foudre Apero Races",
+      "desc": "Find more details about our competition here",
+      "alt": "Foudre image desc",
+      "btnLink": "http:\\google.com",
+      "btnTitle": "read more",
+      "btnColor": "btn-primary"
+    },
+    {
+      "src": "https://www.equipecofidis.com/medias/articles/desktop-giro-d-italia-2024-picture-20240430112821.png",
+      "title": "Join us now",
+      "desc": "Find more details about our competition here",
+      "alt": "Foudre image desc",
+      "btnLink": "http:\\google.com",
+      "btnTitle": "read more",
+      "btnColor": "btn-primary"
+    },
+    {
+      "src": "https://www.equipecofidis.com/medias/articles/desktop-team-cofidis-les-ardennaises-et-le-doubs-pour-se-relancer-picture-20240412104158.jpg",
+      "title": "Join us now",
+      "desc": "Find more details about our competition here",
+      "alt": "Foudre image desc",
+      "btnLink": "http:\\google.com",
+      "btnTitle": "read more",
+      "btnColor": "btn-primary"
+    }
+  ];
 
   constructor() {
 
